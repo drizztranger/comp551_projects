c985289c6fce8ab9fb5047a70377e0b5ebb616e8
Simon Geoffroy-Gagnon <simonllpgg@gmail.com>
2019-01-21 18:05:05 -0500

    Completed the 10,000 training, 1000 valid, 164 features test

--- p1/proj1_gradientDescent_regression.py
+++ p1/proj1_gradientDescent_regression.py
@@ -12,6 +12,7 @@ import proj1_data_preprocessing as pp
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+import time
 from numpy import linalg
 from matplotlib.pyplot import figure
 
@@ -39,8 +40,8 @@ def fit_model_closed_form(x, y):
     return np.asarray(w), np.linalg.norm(w, ord=1)
 
 
-def fit_model_gradient(x, y, epochs = 10000, beta = 0.05, eta = 1e-5,\
-                       epsilon = 1e-5):
+def fit_model_gradient(x, y, epochs = 10000, beta = 0.01, eta = 1e-5,\
+                       epsilon = 1e-6):
     err = []
     w = []
     w_norm = []
@@ -59,25 +60,36 @@ def fit_model_gradient(x, y, epochs = 10000, beta = 0.05, eta = 1e-5,\
         if np.linalg.norm((w[-1] - w[-2]), ord=2) < epsilon:
             print('Epsilon value reached after {} epochs'.format(ii))
             return np.asarray(w[-1]),  w_norm
-        if ii%100 == 0:
+        if ii%epochs/100 == 0:
             print(ii)
-            
+
     print('Epoch limit of {} reached'.format(epochs))
 
     return np.asarray(w[-1]), w_norm
 
 
-def main():
+def cf_test():
+    x_trn, y_trn, x_val, y_val, _, _ = \
+    pp.preprocess(trn_len=10000, val_len=1000, tst_len=1000, mst_cmn_wd_len=160,\
+               mst_cmn_wd_start = 0, order = 1)
 
+    w_cf = fit_model_closed_form(x_trn, y_trn)
+    trn_err_cf = get_error(x_trn, y_trn, w_cf)
+    val_err_cf = get_error(x_val, y_val, w_cf)
+
+    print('Trn Err: {}, Val Err: {}'.format(trn_err_cf, val_err_cf))
+
+
+def main():
 
     x_trn, y_trn, x_val, y_val, x_tst, y_tst = \
     pp.preprocess(trn_len=10000, val_len=1000, tst_len=1000, mst_cmn_wd_len=160,\
                mst_cmn_wd_start = 0, order = 1)
-    
+
 #    x_trn, y_trn, x_val, y_val, x_tst, y_tst = \
 #    pp.normalize_all(x_trn, y_trn, x_val, y_val, x_tst, y_tst)
 
-    w_gd, w_gd_norm = fit_model_gradient(x_trn, y_trn, epochs=10000, eta=1e-5)
+    w_gd, w_gd_norm = fit_model_gradient(x_trn, y_trn, epochs=50000, eta=1e-5)
     w_cf, w_cf_norm = fit_model_closed_form(x_trn, y_trn)
 
     w0 = np.zeros(len(w_cf))
@@ -110,27 +122,23 @@ def main():
                          trn_err_gd, val_err_cf, trn_err_cf, val_w0, trn_w0))
     return trn_err_cf, val_err_cf, trn_err_gd, val_err_gd
 
-def cf_test():
-    x_trn, y_trn, x_val, y_val, _, _ = \
-    pp.preprocess(trn_len=10000, val_len=1000, tst_len=1000, mst_cmn_wd_len=160,\
-               mst_cmn_wd_start = 0, order = 1)
-
-    w_cf = fit_model_closed_form(x_trn, y_trn)
-    trn_err_cf = get_error(x_trn, y_trn, w_cf)
-    val_err_cf = get_error(x_val, y_val, w_cf)
-
-    print('Trn Err: {}, Val Err: {}'.format(trn_err_cf, val_err_cf))
-
 if __name__ == '__main__':
+    start = time.time()
     trn_err_cf, val_err_cf, trn_err_gd, val_err_gd = main()
-
+    end = time.time()
+    print((end-start)/60)
 # 164 features
 #GD Val Err: [0.99492486], GD Trn Err: [1.04980198]
 #CF Val Err: [0.9950694], CF Trn Err: [1.04777632]
 #w0 Val Err: [2.07317095], w0 Trn Err: [2.1124615]
 
+#GD Val Err: [0.99492486], GD Trn Err: [1.04980198]
+#CF Val Err: [0.9950694], CF Trn Err: [1.04777632]
+#w0 Val Err: [2.07317095], w0 Trn Err: [2.1124615]
+#1.3634110808372497
 
-
-
-
+#GD Val Err: [0.9954365], GD Trn Err: [1.04878805]
+#CF Val Err: [0.9950694], CF Trn Err: [1.04777632]
+#w0 Val Err: [2.07317095], w0 Trn Err: [2.1124615]
+#4.187006946404775
 
